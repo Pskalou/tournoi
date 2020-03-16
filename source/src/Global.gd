8230ba0f86e2b02e1499f68aa6a9c2c6eb7cc637
Pskalou <pascal.padilla@gmail.com>
2020-03-16 14:29:07 +0100

    Gloab -> classe + docstring

--- source/src/Global.gd
+++ source/src/Global.gd
@@ -1,58 +1,83 @@
-# Classe globale chargée automatiquement lors du lancement de l'application 
-# cette classe est globale (cf menu Projet > Paramètres du projet... > Autoload)
-# car nous avons coché le design pattern "Singleton"
+# La Classe globale chargée automatiquement lors du lancement de l'application.
+# Elle stocke les informations essentielles et nécessaires à toutes les classes.
+#
+# Cette classe est globale et utilise le principe Singleton de Godot.
+# (cf menu `Projet > Paramètres du projet... > Autoload` et cocher le design pattern "*Singleton*")
+#
+# Le nom *GlobalClass* est utilisé uniquement pour générer la doc
+class_name Global_
+
 extends Node
 
 
-# paramètres par défaut lors du lancement de l'application
-var _total_players= 6
-var is_saved= true
+# attribut privé : nombre total de joueur (par défaut = 6)
+var _total_players:int= 6
 
 
-# lors d'un tournoi, game référence une instance de GameGenerator => algorithme de répartition des matchs
-var game
-
-# lors d'un tournoi, score référence une instance de ScoreManager => gestion du score
-var score
-
-# référence une instance de Round_buttons
-# => pour un tour donné (round) contient et gère les duel_boutons
-# => modifie l'état instantané du tournoi en fonction des clics
-var round_buttons
-
-# tableau qui contient, pour un tour donné, la liste des duel_boutons
-var round_buttons_list
-
-# définition du node racine qui va contenir toute les scènes de l'application
-var main
+# Cet attribut indique si le tournoi a été modifié depuis la dernière sauvegarde.
+# Si un risque de perdre les résultats en cours, une fenêtre modale  apparaît
+# et demande si on est sûr d'être certain.
+#
+# Valeur par défaut : True
+var is_saved:bool= true
 
 
-# instanciation des scènes
-# chaque scène est donc présente pendant l'application,
-# mais toutes ne sont pas visibles en même temps
+# L'attribut *game* référence une instance de `GameGenerator`.
+# C'est le moyen de déterminer l'`adversaire_id` en fonction du `round` et de `player_id`.
+var game:Game_generator
+
+
+# L'attribut `score` référence une instance de **ScoreManager**
+# C'est la classe dédiée à la gestion du score.
+var score:Score_manager
+
+
+# L'attribut `round_button` référence une instance de `Round_buttons` :
+#
+# * contient et gère les `duel_boutons` (pour chaque round)
+# * modifie l'état instantané du tournoi en fonction des clics
+# * est détruite (queue_free) puis réinstanciée pour chaque roud
+var round_buttons:Round_buttons
+
+
+# Tableau de `int` qui contient, pour un tour donné, la liste des `duel_boutons`.
+var round_buttons_list:Array
+
+
+# Node racine de l'application. Ce node va contenir toute les scènes de l'application.
+var main:MarginContainer
+
+
+# Instance de la scène menu d'accueil de l'application. Visible ou pas en fonction de l'interface souhaitée.
 var accueil_menu= 			preload("res://accueil_menu.tscn").instance()
+
+
+# Instance de la scène menu de configuration d'un nouveau tournoi. Visible ou pas en fonction de l'interface souhaitée.
 var new_match_menu= 		preload("res://new_match_menu.tscn").instance()
+
+
+# Instance de la scène qui affiche le tournoi, les boutons et le score en direct.
+# Visible ou pas en fonction de l'interface souhaitée.
 var game_set_results_menu= 	preload("res://game_set_results_menu.tscn").instance()
 
 
-func set_total_players(nombre_de_joueurs):
-	# setter pour le nombre de joueurs dans le tournoi actuel
+# Setter pour définir le nombre de joueurs dans le tournoi actuel.
+func set_total_players(nombre_de_joueurs:int) -> void:
 	_total_players= nombre_de_joueurs
 	
 
-func get_total_players():
-	# getter pour le nombre de joueurs dans le tournoi actuel
-
+# Getter qui retourne le nombre de joueurs dans le tournoi actuel.
+func get_total_players() -> int:
 	return _total_players
 
 
-func button_hover(button):
-	# changement de style lors du survol d'un bouton de type TextureButton
-
+# Méthode pour définir et homogénéiser l'ensemble des changement de style
+# à appliquer lors du **survol d'un bouton** de type TextureButton.
+func button_hover(button:TextureButton) -> void:
 	button.modulate= Color(1,1,1,0.5)
 
 
+# Méthode pour définir et homogénéiser l'ensemble des changement de style
+# à appliquer pour réinitialiser le bouton de type TextureButton (après un survol de la souris).
 func button_init(button):
-	# réinitialisation du syle des boutons TextureButton après le survol de la souris
-
 	button.modulate= Color(1,1,1,1)
