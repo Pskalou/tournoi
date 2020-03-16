ae26f04fc419dcb05e155744dffb80bc853257b4
Pskalou <pascal.padilla@gmail.com>
2020-03-16 15:23:51 +0100

    privatisation de tous les attributs/méthodes

--- source/src/new_match_menu.gd
+++ source/src/new_match_menu.gd
@@ -1,33 +1,38 @@
 extends MarginContainer
 
-var more_btn
-var less_btn
-var back_btn
-var ok_btn
+var _more_btn
+var _less_btn
+var _back_btn
+var _ok_btn
 
-var total_text
+
+var _total_text
+
 
 func _ready():
 	# définition des raccourcis boutons
-	ok_btn= $VBoxContainer/HBoxContainer/VBoxContainer2/ok_btn
-	back_btn= $VBoxContainer/HBoxContainer/VBoxContainer2/back_btn
-	less_btn= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/less_btn
-	more_btn= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/more_btn
-	
-	total_text= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/total_text
+	_ok_btn= $VBoxContainer/HBoxContainer/VBoxContainer2/ok_btn
+	_back_btn= $VBoxContainer/HBoxContainer/VBoxContainer2/back_btn
+	_less_btn= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/less_btn
+	_more_btn= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/more_btn
+
+	_total_text= $VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/total_text
 	
 	# update affichage
-	total_text.set_text(str(Global.get_total_players()))
+	_total_text.set_text(str(Global.get_total_players()))
+
 
 func _alert():	
 	modulate= Color(1,1,1,0.25)
 	var alert= preload("res://not_saved_pop_up_menu.tscn").instance()
 	Global.main.add_child(alert)
 
+
 func _on_ok_btn_pressed():
 	Global.accueil_menu.visible= false
 	Global.new_match_menu.visible= false
 	Global.game_set_results_menu.visible= true
+
 
 func _on_back_btn_pressed():
 	Global.accueil_menu.visible= true
@@ -40,7 +45,7 @@ func _on_more_btn_pressed():
 	augmenter le nombre de joueur et actualiser l'affichage
 	"""
 	if not Global.is_saved:
-		Global.button_init(more_btn)
+		Global.button_init(_more_btn)
 		_alert()
 		return null
 
@@ -54,7 +59,7 @@ func _on_more_btn_pressed():
 	game_set_results_menu.init()
 	
 	# update affichage
-	total_text.set_text(str(total_players))
+	_total_text.set_text(str(total_players))
 
 
 func _on_less_btn_pressed():
@@ -63,7 +68,7 @@ func _on_less_btn_pressed():
 	actualiser l'affichage
 	"""
 	if not Global.is_saved:
-		Global.button_init(less_btn)
+		Global.button_init(_less_btn)
 		_alert()
 		return null
 
@@ -86,25 +91,28 @@ func _on_less_btn_pressed():
 	game_set_results_menu.init()
 	
 	# update affichage
-	total_text.set_text(str(total_players))
+	_total_text.set_text(str(total_players))
 
 
 func _on_more_btn_mouse_entered():
-	Global.button_hover(more_btn)
+	Global.button_hover(_more_btn)
 func _on_more_btn_mouse_exited():
-	Global.button_init(more_btn)
+	Global.button_init(_more_btn)
+
 
 func _on_less_btn_mouse_entered():
-	Global.button_hover(less_btn)
+	Global.button_hover(_less_btn)
 func _on_less_btn_mouse_exited():
-	Global.button_init(less_btn)
+	Global.button_init(_less_btn)
+
 
 func _on_ok_btn_mouse_entered():
-	Global.button_hover(ok_btn)
+	Global.button_hover(_ok_btn)
 func _on_ok_btn_mouse_exited():
-	Global.button_init(ok_btn)
+	Global.button_init(_ok_btn)
+
 
 func _on_back_btn_mouse_entered():
-	Global.button_hover(back_btn)
+	Global.button_hover(_back_btn)
 func _on_back_btn_mouse_exited():
-	Global.button_init(back_btn)
+	Global.button_init(_back_btn)
